153430474618a0657994313c75389a49cfa9b6e4
Itsprime <itsprimesuarez@gmail.com>
2023-07-18 22:19:39 -0700

    working javascript

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// const passwordBox = document.getElementsByName('password')
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
-const includeUppercase = document.getElementById('uppercase')
-const includeLowercase = document.getElementById('lowercase')
-const includeNumber = document.getElementById('number')
-const includeSymbol = document.getElementById('symbol')
-const password = document.getElementsByClassName('password')
+const includeUppercaseElement = document.getElementById('uppercase')
+const includeLowercaseElement = document.getElementById('lowercase')
+const includeNumberElement = document.getElementById('number')
+const includeSymbolElement = document.getElementById('symbol')
+const passwordElement = document.getElementById('password')
 const form = document.getElementById('passwordGeneratorForm')
 
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
-
 
 function syncCharacterAmount(e) {
     const value = e.target.value
@@ -18,16 +16,43 @@ function syncCharacterAmount(e) {
     characterAmountRange.value = value
 }
 
-function generateCharCode(low, high){
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const character_range = characterAmountNumber.value
+    const includeLowercase = includeLowercaseElement.value
+    const includeUppercase = includeUppercaseElement.value
+    const includeNumbers = includeNumberElement.value
+    const includeSymbols = includeSymbolElement.value
+    const password = createPassword(includeLowercase, includeNumbers, includeSymbols, includeUppercase, character_range)
+    passwordElement.innerText = password
+})
+
+const LOWERCASE_CHAR_CODE = generateCharCode(97, 122)
+const UPPERCASE_CHAR_CODE = generateCharCode(65, 90)
+const NUMBER_CHAR_CODE = generateCharCode(48, 57)
+const SYMBOL_CHAR_CODE = generateCharCode(33, 47).concat(generateCharCode(58, 126))
+console.log(SYMBOL_CHAR_CODE)
+
+function generateCharCode(low, high) {
     const arraylist = []
-    for(let i = low; i < high; i++) {
+    for (let i = low; i <= high; i++) {
         arraylist.push(i)
     }
     return arraylist
 }
 
-
-
-
-
-
+function createPassword(includeLowercase, includeNumber, includeSymbols, includeUppercase, character_range) {
+    let passwordCreated = []
+    if (includeLowercase) passwordCreated = passwordCreated.concat(LOWERCASE_CHAR_CODE)
+    if (includeUppercase) passwordCreated = passwordCreated.concat(UPPERCASE_CHAR_CODE)
+    if (includeNumber) passwordCreated = passwordCreated.concat(NUMBER_CHAR_CODE)
+    if (includeSymbols) passwordCreated = passwordCreated.concat(SYMBOL_CHAR_CODE)
+    console.log("working" + passwordCreated)
+    const passwordCharacters = []
+    for (let i = 0; i < character_range; i++) {
+        const characterCode = passwordCreated[Math.floor(Math.random() * passwordCreated.length)]
+        passwordCharacters.push(String.fromCharCode(characterCode))
+    }
+    return passwordCharacters.join('')
+}
